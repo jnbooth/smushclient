4a7b025972f70b77d754383f501b110cca1e23e9
Joshua Booth <joshua.n.booth@gmail.com>
2024-09-17 10:55:53 -0700

    pass additional arguments to build.sh

--- smushclient-qt/build.sh
+++ smushclient-qt/build.sh
@@ -4,13 +4,14 @@ CONFIG="Debug"
 
 if [ "$1" = "--release" ]; then
   CONFIG="Release"
+  shift
 fi
 
 DIR="build/$CONFIG"
 
 cd "$(dirname "$0")"
 cmake -S . -B $DIR
-cmake --build $DIR --config $CONFIG
+cmake --build $DIR --config $CONFIG $@
 
 if [ -z "$RUN" ]; then
   exit
