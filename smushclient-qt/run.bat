f86405a0a2e847bf28767d338e7c7bff0a03be3b
Joshua Booth <emrys.myrddhin@gmail.com>
2024-12-15 11:02:25 -0800

    in run.bat, don't run if build fails

--- smushclient-qt/run.bat
+++ smushclient-qt/run.bat
@@ -1,5 +1,4 @@
 cd /D "%~dp0"
 
 cmake.exe -S . -B build\Release
-cmake.exe --build build\Release --config Release --target SmushClient
-build\Release\Release\SmushClient.exe
+cmake.exe --build build\Release --config Release --target SmushClient && build\Release\Release\SmushClient.exe
