3c9eec6ddaf43edb8f71c0920a00e858d1aba5f3
Joshua Booth <joshua.n.booth@gmail.com>
2025-01-01 16:17:55 -0800

    remove SmushClient.app during build

--- smushclient-qt/build.sh
+++ smushclient-qt/build.sh
@@ -10,5 +10,6 @@ fi
 BUILDDIR="build/$CONFIG"
 
 cd "$(dirname "$0")"
+rm -rf $BUILDDIR/SmushClient.app
 cmake -S . -B $BUILDDIR
 cmake --build $BUILDDIR --config $CONFIG --target update_translations --target SmushClient $@
