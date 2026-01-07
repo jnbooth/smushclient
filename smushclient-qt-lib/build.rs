4d46e648b4edc6f87cc8789a80d50b6f6f10cc9a
Joshua Booth <joshua.n.booth@gmail.com>
2026-01-06 17:19:35 -0800

    bump cxx-qt

--- smushclient-qt-lib/build.rs
+++ smushclient-qt-lib/build.rs
@@ -12,43 +12,28 @@ macro_rules! include_header {
     };
 }
 
-struct HeaderBuilder {
-    dir: PathBuf,
+trait CxxQtBuilderExt {
+    fn cpp_files(self, cpp_files: &[&str]) -> Self;
 }
 
-impl HeaderBuilder {
-    pub fn new() -> Self {
-        let dir = PathBuf::from(env::var("OUT_DIR").unwrap())
-            .join("include")
-            .join("smushclient-qt-lib");
-        Self { dir }
-    }
-
-    pub fn create_header_dir(&self) {
-        fs::create_dir_all(&self.dir).expect("Failed to create include directory");
-    }
-
-    pub fn write_headers(&self, files: &[(&[u8], &OsStr)]) {
-        for &(file_contents, file_name) in files {
-            let out_path = self.dir.join(file_name);
-            let mut header = File::create(out_path).expect("Could not create header");
-            header
-                .write_all(file_contents)
-                .expect("Could not write header");
+impl CxxQtBuilderExt for CxxQtBuilder {
+    fn cpp_files(mut self, cpp_files: &[&str]) -> Self {
+        for cpp_file in cpp_files {
+            self = self.cpp_file(cpp_file);
         }
+        self
     }
 }
 
-impl AsRef<Path> for HeaderBuilder {
-    fn as_ref(&self) -> &Path {
-        &self.dir
-    }
-}
-
+#[allow(clippy::needless_borrows_for_generic_args)]
 fn main() {
-    let header_dir = HeaderBuilder::new();
-    header_dir.create_header_dir();
-    header_dir.write_headers(&[
+    let header_dir = PathBuf::from(env::var("OUT_DIR").unwrap())
+        .join("include")
+        .join("smushclient-qt-lib");
+
+    fs::create_dir_all(&header_dir).expect("Failed to create include directory");
+
+    let header_files: &[(&[u8], &OsStr)] = &[
         include_header!("include/core/qlist/qlist_private.h"),
         include_header!("include/core/qlist/qlist_qtextlength.h"),
         include_header!("include/core/qlist/qlist.h"),
@@ -63,43 +48,42 @@ fn main() {
         include_header!("include/gui/qtextcharformat.h"),
         include_header!("include/gui/qtextformat.h"),
         include_header!("include/gui/qtextlength.h"),
-    ]);
-
-    let cpp_files = &[
-        "src/core/qlist/qlist.cpp",
-        "src/core/qmap/qmap.cpp",
-        "src/core/qvariant/qvariant.cpp",
-        "src/gui/qbrush.cpp",
-        "src/gui/qtextcharformat.cpp",
-        "src/gui/qtextformat.cpp",
-        "src/gui/qtextlength.cpp",
     ];
 
-    let rust_files = &[
-        "src/core/qmap/qmap_i32_qvariant.rs",
-        "src/core/qlist/qlist_qtextlength.rs",
-        "src/core/qt.rs",
-        "src/core/qvariant/qvariant_qbrush.rs",
-        "src/core/qvariant/qvariant_qtextformat.rs",
-        "src/core/qvariant/qvariant_qtextlength.rs",
-        "src/gui/qbrush.rs",
-        "src/gui/qtextcharformat.rs",
-        "src/gui/qtextformat.rs",
-        "src/gui/qtextlength.rs",
-    ];
+    for &(file_contents, file_name) in header_files {
+        let out_path = header_dir.join(file_name);
+        let mut header = File::create(out_path).expect("Could not create header");
+        header
+            .write_all(file_contents)
+            .expect("Could not write header");
+    }
 
     CxxQtBuilder::new()
         .include_prefix("private")
         .crate_include_root(Some("include/".to_owned()))
         .include_dir(&header_dir)
         .qt_module("Gui")
-        .files(rust_files)
-        .cc_builder(move |cc| {
-            for cpp_file in cpp_files {
-                cc.file(cpp_file);
-                println!("cargo::rerun-if-changed={cpp_file}");
-            }
-        })
+        .cpp_files(&[
+            "src/core/qlist/qlist.cpp",
+            "src/core/qmap/qmap.cpp",
+            "src/core/qvariant/qvariant.cpp",
+            "src/gui/qbrush.cpp",
+            "src/gui/qtextcharformat.cpp",
+            "src/gui/qtextformat.cpp",
+            "src/gui/qtextlength.cpp",
+        ])
+        .files(&[
+            "src/core/qmap/qmap_i32_qvariant.rs",
+            "src/core/qlist/qlist_qtextlength.rs",
+            "src/core/qt.rs",
+            "src/core/qvariant/qvariant_qbrush.rs",
+            "src/core/qvariant/qvariant_qtextformat.rs",
+            "src/core/qvariant/qvariant_qtextlength.rs",
+            "src/gui/qbrush.rs",
+            "src/gui/qtextcharformat.rs",
+            "src/gui/qtextformat.rs",
+            "src/gui/qtextlength.rs",
+        ])
         .build()
         .reexport_dependency("cxx-qt-lib")
         .export();
--- smushclient-qt/rust/build.rs
+++ smushclient-qt/rust/build.rs
@@ -1,8 +1,15 @@
+use std::ffi::OsStr;
 use std::fs::{self, File};
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use cxx_qt_build::CxxQtBuilder;
+
+macro_rules! include_header {
+    ($file:literal) => {
+        (include_bytes!($file), Path::new($file).file_name().unwrap())
+    };
+}
 
 fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
@@ -12,24 +19,23 @@ fn main() {
 
     fs::create_dir_all(&header_dir).expect("Could not create header dir");
 
-    for (file_contents, file_name) in [
-        (include_str!("../cpp/bridge/document.h"), "document.h"),
-        (include_str!("../cpp/bridge/forward.h"), "forward.h"),
-        (include_str!("../cpp/bridge/timekeeper.h"), "timekeeper.h"),
-        (
-            include_str!("../cpp/bridge/variableview.h"),
-            "variableview.h",
-        ),
-    ] {
-        let h_path = header_dir.join(file_name);
-        let mut header = File::create(h_path).expect("Could not create header");
-        write!(header, "{file_contents}").expect("Could not write header");
+    let header_files: &[(&[u8], &OsStr)] = &[
+        include_header!("../cpp/bridge/document.h"),
+        include_header!("../cpp/bridge/forward.h"),
+        include_header!("../cpp/bridge/timekeeper.h"),
+        include_header!("../cpp/bridge/variableview.h"),
+    ];
+
+    for &(file_contents, file_name) in header_files {
+        let out_path = header_dir.join(file_name);
+        let mut header = File::create(out_path).expect("Could not create header");
+        header
+            .write_all(file_contents)
+            .expect("Could not write header");
     }
 
     CxxQtBuilder::new()
-        .cc_builder(|cc| {
-            cc.include(&header_dir);
-        })
+        .include_dir(&header_dir)
         .files([
             "src/ffi/api_code.rs",
             "src/ffi/audio.rs",
