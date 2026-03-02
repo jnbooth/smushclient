d9b2ada288beaab8b075279c85b0a332f245f617
Joshua Booth <joshua.n.booth@gmail.com>
2026-03-01 18:55:10 -0800

    Revert "use .copied()"
    This reverts commit e6f0c2f39cf20b76617f101dbdeef0aac005c20d.

--- smushclient-qt/rust/src/text_formatter.rs
+++ smushclient-qt/rust/src/text_formatter.rs
@@ -64,7 +64,7 @@ impl TextFormatter {
         }
 
         self.ansi.clear();
-        for color in world.ansi_colours.iter().copied() {
+        for &color in &world.ansi_colours {
             self.ansi.insert(color, foreground_format(color));
         }
     }
