2e6e1301bbd7830b6903c254a5451fad299b7414
Joshua Booth <joshua.n.booth@gmail.com>
2026-04-11 20:52:52 -0700

    fix clamped_cast

--- smushclient-qt/cpp/casting.h
+++ smushclient-qt/cpp/casting.h
@@ -1,16 +1,31 @@
 #pragma once
-#include <algorithm>
 #include <limits>
 
-template<typename T, typename U>
-constexpr T
-clamped_cast(U n) noexcept
+template<typename To, typename From>
+constexpr To
+clamped_cast(From n) noexcept
+  requires(std::numeric_limits<From>::is_integer,
+           std::numeric_limits<To>::is_integer)
 {
-  return static_cast<T>(
-    std::clamp<U>(n,
-                  static_cast<U>(std::numeric_limits<T>::min()),
-                  static_cast<U>(std::numeric_limits<T>::max())));
+  using from_limits = std::numeric_limits<From>;
+  using to_limits = std::numeric_limits<To>;
+
+  constexpr const From min = !from_limits::is_signed || !to_limits::is_signed
+                               ? 0
+                             : from_limits::digits >= to_limits::digits
+                               ? static_cast<From>(to_limits::min())
+                               : from_limits::min();
+
+  constexpr const From max = from_limits::digits >= to_limits::digits
+                               ? static_cast<From>(to_limits::max())
+                               : from_limits::max();
+
+  return (n < min)   ? to_limits::min()
+         : (n > max) ? to_limits::max()
+                     : static_cast<To>(n);
 }
+
+constexpr const size_t test = 0;
 
 template<typename Source, typename EnumValue>
 void (Source::* enum_slot_cast(void (Source::*slot)(EnumValue)))(int)
