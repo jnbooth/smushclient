3fda2687f43c3a7df5b95e4a569f694c7f4ecded
Joshua Booth <joshua.n.booth@gmail.com>
2026-04-14 15:25:11 -0700

    reword stubs

---
+++ lua-api/stub.d.lua
@@ -0,0 +1,225 @@
+---@meta
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function Accelerator(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return {}
+function AcceleratorList(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function AcceleratorTo(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function AddMapperComment(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 30064
+function AddSpellCheckWord(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function AddToMapper(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function Bookmark(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return nil
+function Debug(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function DeleteAllMapItems(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function DeleteLastMapItem(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function DoCommand(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function FlashIcon(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function GetCustomColourBackground(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function GetCustomColourName(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return {}
+function GetInternalCommandList(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function GetMapping(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function GetMappingCount(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return nil
+function GetMappingItem(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return ""
+function GetMappingString(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return -1
+function GetNoteColour(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return ""
+function GetNotes(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function GetRemoveBacktracks(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function GetRemoveMapReverses(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function GetScriptTime(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function GetTrace(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function GetUdpPort(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function Help(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function Info(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function InfoBackground(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function InfoClear(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function InfoColour(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function InfoFont(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function Mapping(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function MoveMainWindow(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function MoveNotepadWindow(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function MoveWorldWindow(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function MoveWorldWindowX(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function ReadNamesFile(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function Redraw(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function Repaint(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function ResetIP(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetCustomColourBackground(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function SetCustomColourName(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetCustomColourText(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetFrameBackgroundColour(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetMapping(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetNoteColour(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetNotes(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetRemoveBacktracks(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetRemoveMapReverses(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function SetToolBarPosition(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetTrace(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function SetUnseenLines(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function ShiftTabCompleteItem(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function ShowInfoBar(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return nil
+function SpellCheck(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 30064
+function SpellCheckCommand(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return nil
+function SpellCheckDlg(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function TraceOut(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return 0
+function TranslateDebug(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+---@return false
+function Transparency(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function utils.reload_global_prefs(...) end
+
+---Stub. Provided for compatibility with MUSHclient.
+function utils.sendtofront(...) end
