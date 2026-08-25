6ac13878aa3e7fa096044b371875ec2e80797548
Joshua Booth <joshua.n.booth@gmail.com>
2026-08-25 14:14:16 -0700

    handle WindowText if rectangle goes to 0,0

--- smushclient-qt/cpp/scripting/miniwindow/miniwindow.cpp
+++ smushclient-qt/cpp/scripting/miniwindow/miniwindow.cpp
@@ -433,7 +433,11 @@ MiniWindow::drawText(const QFont& font,
   Painter painter(this, color);
   painter.setFont(font);
   QRectF boundingRect;
-  painter.drawText(normalize(rect), 0, text, &boundingRect);
+  const QRectF normalized =
+    rect.bottomRight().isNull()
+      ? QRectF(rect.topLeft(), this->rect().bottomRight())
+      : normalize(rect);
+  painter.drawText(normalized, 0, text, &boundingRect);
   return boundingRect;
 }
 
